90cb44a0cf67c6e7d0025ee9e5d7505c748a4e6d
Swarnali Mitra <swarnalighosh1974@gmail.com>
2025-12-23 13:51:53 +0530

    Add files via upload

---
+++ Basic/Pattern.java
@@ -0,0 +1,31 @@
+//Patterns using print statement
+
+class Pattern{
+    public static void main(String[] args) {
+
+        //Left Sided Triangle
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *"); 
+        System.out.println("* * * * *");
+
+        System.out.println("");
+
+        //Inverted Triangle
+        System.out.println("* * * * *"); 
+        System.out.println("* * * *");   
+        System.out.println("* * *"); 
+        System.out.println("* *");
+        System.out.println("*");
+
+        System.out.println("");
+
+        //Right Sided Triangle
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *"); 
+        System.out.println("* * * * *");        
+    }
+}
